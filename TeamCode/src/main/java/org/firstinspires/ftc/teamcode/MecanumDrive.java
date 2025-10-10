56ea5d4c1089be05e171a59a242696f712d2b530
FBNF <ftcpeoria@gmail.com>
2025-10-10 18:56:30 -0500

    'Done with angular ramp logger tuning'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -63,13 +63,13 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
 
         // drive model parameters
-        public double inPerTick = 0.00295;
-        public double lateralInPerTick = inPerTick;
-        public double trackWidthTicks = 0;
+        public double inPerTick = 0.00294;
+        public double lateralInPerTick = 0.0021223765249582813;
+        public double trackWidthTicks = 5338.961810462498;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
+        public double kS = 0.9862971243501559;//1.6225290253277156;
+        public double kV = 0.0005729172663920725;//0.0005770632627021179;
         public double kA = 0;
 
         // path profile parameters (in inches)
