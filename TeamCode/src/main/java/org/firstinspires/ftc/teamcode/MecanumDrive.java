46a227201331742579148d29ba2d4cae4927d914
FBNF Coach <ftcpeoria@gmail.com>
2025-10-11 14:56:03 -0500

    Done with ManualFeedbackTuner and fixed the loose screw for the left front motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -64,13 +64,13 @@ public final class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = 0.00294;
-        public double lateralInPerTick = 0.0021223765249582813;
-        public double trackWidthTicks = 5338.961810462498;
+        public double lateralInPerTick = 0.002093805374626218;
+        public double trackWidthTicks = 3850.3727578430767;
 
         // feedforward parameters (in tick units)
         public double kS = 0.9862971243501559;//1.6225290253277156;
-        public double kV = 0.0005729172663920725;//0.0005770632627021179;
-        public double kA = 0;
+        public double kV = 0.00052; //0.0005729172663920725;//0.0005770632627021179;
+        public double kA = 0.0000813;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
