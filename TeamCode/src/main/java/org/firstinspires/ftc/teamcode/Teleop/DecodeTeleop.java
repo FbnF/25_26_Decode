192ee1fd60bf89741a7a5e7cf7a76c67c7ca2cc8
FBNF Coach <ftcpeoria@gmail.com>
2025-10-12 15:00:35 -0500

    For finer motor control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/DecodeTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/DecodeTeleop.java
@@ -1,18 +1,30 @@
 package org.firstinspires.ftc.teamcode.Teleop;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+
 @TeleOp(group = "Teleop")
 public class DecodeTeleop extends LinearOpMode {
     private DcMotorEx ArmMotor;
     private int PosPowReq =0;
     private int NegPowReq =0;
 
+
+    FtcDashboard dashboard;
+
     @Override
     public void runOpMode() throws InterruptedException {
+        // - - - Set up dashboard telemetry - - - //
+        dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
         ArmMotor = hardwareMap.get(DcMotorEx.class, "ArmMotor");
         ArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         ArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -45,6 +57,17 @@ public class DecodeTeleop extends LinearOpMode {
                 PosPowReq=0;
                 ArmMotor.setPower(0);
             }
+
+            // right stick y controls the full range of power when it is absolute
+            // value greater than 0.1
+            if (Math.abs(gamepad1.right_stick_y)> 0.1 ) {
+                NegPowReq=0;
+                PosPowReq=0;
+                ArmMotor.setPower(gamepad1.right_stick_y);
+            }
+
+            telemetry.addData("Current  Power level: ", ArmMotor.getPowerFloat());
+            telemetry.update();
         }
 
 
