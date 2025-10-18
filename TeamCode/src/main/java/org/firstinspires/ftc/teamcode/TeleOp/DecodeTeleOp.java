6ac67073742697033800b52661b4b6d9f2ba20f7
FBNF <ftcpeoria@gmail.com>
2025-10-17 20:52:08 -0500

    Added, initial velocity formula and april tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DecodeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DecodeTeleOp.java
@@ -6,27 +6,112 @@ import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
 import org.firstinspires.ftc.teamcode.MecanumDrive; // Make sure this import is correct for your project
 
+import java.util.List;
+
 @TeleOp(group = "DecodeTeleop")
-public class DecodeTeleOp extends LinearOpMode{
+public class DecodeTeleOp extends LinearOpMode {
+    MecanumDrive drive;
+    double speedFactor;
+    private static final boolean USE_WEBCAM = true; // true for webcam, false for phone camera
+    private VisionPortal visionPortal;
+
+    private AprilTagProcessor aprilTag;
+
+    //DcMotorEx intakeMotor;
+    //DcMotorEx launchMotor;
+    //double launchPower;
+
+    //shooter velocity constants
+    double g = 9.8; //m/s^2
+    double x;
+    double Feta = 89.98;
+    double HGoal = 0.984;//in meters
+    double HShoot = 0.248;//in meters
+
+    double VelOfShooter;
+
+    // boolean isIntakeRunning;
     @Override
     public void runOpMode() {
         // Initialize the drive class
-        MecanumDrive drive = new MecanumDrive(hardwareMap,new Pose2d(0, 0, 0));
-        DcMotor launchMotor;
-        double speedFactor = 0.5;
+
+        drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        speedFactor = 0.5;
+
+        initAprilTag();
+
+         /*
+         launchPower = 0.0;
+         intakeMotor=hardwareMap.get(DcMotorEx.class, "IntakeMotor");
+         launchMotor=hardwareMap.get(DcMotorEx.class,"LaunchMotor");
+         launchMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+         isIntakeRunning = true;
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+*/
         // Wait for the start button to be pressed
         waitForStart();
+        //intakeMotor.setPower(1.0);
+        //launchMotor.setPower(launchPower);
 
         // Ensure the op mode is active and the robot is not interrupted
         while (opModeIsActive()) {
             // Get input from the gamepad
             double axial = -gamepad1.right_stick_y * speedFactor; // Invert the y-axis
-            double lateral = gamepad1.left_stick_x * speedFactor;  // Strafe is x-axis
-            double heading = gamepad1.right_stick_x * speedFactor; // Rotation is x-axis of right stick
+            double lateral = -gamepad1.left_stick_x * speedFactor;  // Strafe is x-axis
+            double heading = -gamepad1.right_stick_x * speedFactor; // Rotation is x-axis of right stick
+            x = telemetryAprilTag();
+            if (x == -1) {
+                VelOfShooter = 0;
+            } else {
+                VelOfShooter = Math.sqrt(g * Math.pow(x, 2) / (2 * Math.pow(Math.cos(Feta), 2)) * (x * Math.tan(Feta) - (HGoal - HShoot)));
+            }
+            /*
 
+            //intake system
+            if(gamepad1.right_bumper){
+                if(isIntakeRunning){
+                    intakeMotor.setPower(0);
+                } else {
+                    intakeMotor.setPower(0.5);
+                }
+                isIntakeRunning = !isIntakeRunning;
+            }
+
+            //launch speed
+            if(gamepad1.aWasPressed()){
+                if (launchPower + 0.1 == 1.0){
+                    launchPower = 1.0;
+                }
+                else{
+                    launchPower=launchPower+0.1;
+                }
+
+            }
+            else if (gamepad1.bWasPressed()) {
+                if (launchPower - 0.1 < 0){
+                    launchPower = 0;
+                }
+                else {
+                    launchPower=launchPower-0.1;
+                }
+            }
+            launchMotor.setPower(launchPower);
+
+
+            //launch system
+*/
             PoseVelocity2d drivePower = new PoseVelocity2d(
                     new Vector2d(
                             axial,
@@ -42,6 +127,8 @@ public class DecodeTeleOp extends LinearOpMode{
             telemetry.addData("Axial (Forward/Back)", axial);
             telemetry.addData("Lateral (Strafe)", lateral);
             telemetry.addData("Heading (Turn)", heading);
+            //telemetry.addData("Launch speed", launchMotor.getPower());
+            //telemetry.addData("is intake running?", isIntakeRunning);
             telemetry.update();
 
 
@@ -61,4 +148,75 @@ public class DecodeTeleOp extends LinearOpMode{
             // telemetry.update();
         }
     }
+
+    private void initAprilTag() {
+        aprilTag = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawTagOutline(true)
+                .setDrawTagID(true)
+                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        builder.addProcessor(aprilTag);
+        visionPortal = builder.build();
+    }
+
+    private double telemetryAprilTag() {
+        List<AprilTagDetection> detections = aprilTag.getDetections();
+
+        if (detections.isEmpty()) {
+            telemetry.addLine("🟥 AprilTag: NOT DETECTED");
+            telemetry.addLine("Make sure the tag is visible to the camera.");
+            return (-1);
+        }
+
+        telemetry.addLine("🟩 AprilTag: DETECTED");
+        telemetry.addData("Total Tags Seen", detections.size());
+
+        for (AprilTagDetection tag : detections) {
+            // Map tag ID to name
+            String tagName;
+            switch (tag.id) {
+                case 21:
+                    tagName = "GPP (ID 21)";
+                    break;
+                case 22:
+                    tagName = "PGP (ID 22)";
+                    break;
+                case 23:
+                    tagName = "PPG (ID 23)";
+                    break;
+                default:
+                    tagName = "Unknown (" + tag.id + ")";
+                    break;
+            }
+
+            telemetry.addLine("------------------------------------");
+            telemetry.addData("Tag", tagName);
+
+            if (tag.metadata != null) {
+                telemetry.addData("Position (in)",
+                        String.format("X: %.1f  Y: %.1f  Z: %.1f", tag.ftcPose.x, tag.ftcPose.y, tag.ftcPose.z));
+                telemetry.addData("Orientation (deg)",
+                        String.format("Yaw: %.1f  Pitch: %.1f  Roll: %.1f",
+                                tag.ftcPose.yaw, tag.ftcPose.pitch, tag.ftcPose.roll));
+                telemetry.addData("Range/Bearing/Elev",
+                        String.format("%.1f in, %.1f°, %.1f°",
+                                tag.ftcPose.range, tag.ftcPose.bearing, tag.ftcPose.elevation));
+            } else {
+                telemetry.addData("Tag Center (px)",
+                        String.format("(%.0f, %.0f)", tag.center.x, tag.center.y));
+            }
+            return (tag.ftcPose.y);
+
+        }
+        return(0);
+    }
+
 }
