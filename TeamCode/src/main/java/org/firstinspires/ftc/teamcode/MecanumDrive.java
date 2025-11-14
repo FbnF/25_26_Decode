876791283b3af2b3fbd7fdb7b8a71df170758985
bobis <josephwaizy@gmail.com>
2025-11-13 19:34:47 -0600

    RR tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -63,13 +63,13 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
         // drive model parameters
-        public double inPerTick = 0;
+        public double inPerTick = 0.002934;
         public double lateralInPerTick = 0;
         public double trackWidthTicks = 0;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;//1.6225290253277156;
-        public double kV = 0; //0.0005729172663920725;//0.0005770632627021179;
+        public double kS = 0.965;//1.6225290253277156;
+        public double kV = 0.0005727; //0.0005729172663920725;//0.0005770632627021179;
         public double kA = 0;
 
         // path profile parameters (in inches)
