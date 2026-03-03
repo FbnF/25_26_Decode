393f0cdb560e9117eceec5988831e93560ff63c8
Joseph <josephwaizy@gmail.com>
2026-03-02 19:40:38 -0600

    updated teleops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainCode/TeleOpMainRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainCode/TeleOpMainRed.java
@@ -477,6 +477,12 @@ public class TeleOpMainRed extends LinearOpMode {
                     intakePower = 1;
                 }
 
+                if(gamepad1.dpad_left){
+                    drive.setDrivePowers(new PoseVelocity2d(new Vector2d(axial, lateral), -0.1));
+                } else if (gamepad1.dpad_right){
+                    drive.setDrivePowers(new PoseVelocity2d(new Vector2d(axial, lateral), 0.1));
+                }
+
                 // ---------------- INTAKE ----------------
                 boolean rbEdge = gamepad2.right_bumper && !prevRB;
                 if (rbEdge) {
@@ -530,6 +536,9 @@ public class TeleOpMainRed extends LinearOpMode {
                 if(!spunUpOk && !isLoaded){
                     feedPower = 0.0;
                 }
+
+
+
                 feedServo.setPower(feedPower);
 
                 // If Y pressed when not allowed (not at speed OR no-shot zone), flash gold
